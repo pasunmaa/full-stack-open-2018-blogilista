4c25b319ceef37dc27e7e72cad95d73fd982a122
Petri Asunmaa <petrieas@gmail.com>
2018-07-20 15:04:35 +0300

    Tehtävä 4.4 valmis

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, item) => total + item.likes, 0)
+  /* const totalLikesCount = blogs.reduce((total, item) => {return(total + item.likes)}, 0)
+  console.log('total = ', totalLikesCount)
+  return totalLikesCount */
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
