e7b6486e3933181264633b2d9d47a2fe7b6f4a9c
Petri Asunmaa <petrieas@gmail.com>
2018-09-02 14:34:56 +0300

    Release 1.0

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http')
 const express = require('express')
-const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
